2003dcfdfc650c3f59983b9ea337aa008692d0aa
KhrystynaPilip <tina.hristi@gmail.com>
2024-12-02 15:21:12 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,4 @@
-const sortByDescendingFriendCount = users => users.slice().sort((a, b) => b.friends.length - a.friends.length);
+const sortByDescendingFriendCount = (users) => users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 // Перевірка функції
 console.log(
